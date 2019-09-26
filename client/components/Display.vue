3dd68ff912475026879aee355d6d8bd91a116c49
Hatoma Daiya <phaosdove@gmail.com>
2019-09-26 23:19:09 +0900

    fix Text Position

--- client/components/Display.vue
+++ client/components/Display.vue
@@ -1,12 +1,12 @@
 <template>
   <v-card dark>
-    <v-card-text class="white--text">
-    </v-card-text>
-    <v-card-text class="white--text">
-      {{ $store.state.rsvp.disp_text }}
-    </v-card-text>
-    <v-card-text class="white--text">
-    </v-card-text>
+      <v-card-text class="white--text" />
+      <v-card-title
+        class="justify-center"
+      >
+        {{ $store.state.rsvp.disp_text }}
+      </v-card-title>
+      <v-card-text class="white--text" />
   </v-card>
 </template>
 
